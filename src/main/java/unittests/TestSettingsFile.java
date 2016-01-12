5ef27280302e4a0fca2085bdf4ca95944bb39383
Cladge <theclaridges@googlemail.com>
2016-01-12 00:00:38 +0000

    Forgot test class changes!

--- src/main/java/unittests/TestSettingsFile.java
+++ src/main/java/unittests/TestSettingsFile.java
@@ -2,6 +2,8 @@ package unittests;
 
 import static org.junit.Assert.*;
 
+import java.io.File;
+import java.util.Hashtable;
 import java.util.Properties;
 
 import org.junit.Before;
@@ -14,45 +16,84 @@ import thecollector.model.SettingsFile;
  * Test Settings file.
  * 
  * Expectations:
- * 1. The file has a path.
- * 2. The file path (directory) can be created.
- * 3. The file path exists.
- * 4. The file path (directory) can be deleted.
- * 5. The file can be created.
- * 7. The file can be deleted.
+ * o The settings (properties) file requires a name and path.
+ * o The settings (properties) file points to a valid properties file.
+ * o The file is created if it does not exist.
+ * o A single property can be retrieved.
+ * o All properties can be retrieved.
+ * o A single property can be set.
+ * o The settings (properties) file can be written.
+ * o The settings (properties) file can be deleted.
+ * o The settings (properties) path can be deleted.
  * 
  * @author Ian Claridge
  */
 public class TestSettingsFile {
 	
-	private String path = "C:" + System.getProperty("file.separator") + "testSettingsFilePath";
-	private SettingsFile settingsFile = new SettingsFile(path);
-	private Properties testProperties;
+	private String settingsPath;
+	private String settingsName;
+	private SettingsFile settingsFile;
 
 	@Before
-	public void setUp() throws Exception {
-		this.path = "C:" + System.getProperty("file.separator") + "testSettingsFilePath";
-		this.settingsFile = new SettingsFile(path);
-    	this.testProperties = new Properties();
-		this.testProperties.setProperty("keyTest1", "1");
-		this.testProperties.setProperty("keyTest2", "2");
+	public void setUp() throws Exception {	
+		this.settingsPath = "C:" + System.getProperty("file.separator") + "testSettingsFilePath";
+		this.settingsName = "TestSettings.xml";
 	}
 
 	@Test
-	public void testSettingsFilePath() {
-		assertEquals(this.path, this.settingsFile.getSettingsPath());
-		assertTrue(this.settingsFile.mkdir());
-		assertTrue(this.settingsFile.pathExists());
-		assertTrue(this.settingsFile.deldir());
-	}
-
-	@Test
-	public void testSettingsFile() {
-		assertEquals(this.path, this.settingsFile.getSettingsPath());
-		assertTrue(this.settingsFile.mkdir());
-		assertTrue(this.settingsFile.pathExists());
-		assertTrue(this.settingsFile.create());
+	public void testSettingsFileCanCreate() {
+		this.settingsFile = new SettingsFile(this.settingsPath, this.settingsName);
+		File settingsFilePath = this.settingsFile.getFile();
+		assertTrue(settingsFilePath.exists());
 		assertTrue(this.settingsFile.delete());
-		assertTrue(this.settingsFile.deldir());
+		assertTrue(this.settingsFile.deletePath());
+	}
+	
+	@Test
+	public void testSettingsFileSetProperty() {
+		this.settingsFile = new SettingsFile(this.settingsPath, this.settingsName);
+		assertNull(this.settingsFile.setProperty("key1", "value1"));
+		String newValue = this.settingsFile.setProperty("key1", "value2");
+		assertEquals("value1", newValue);
+		this.settingsFile.delete();
+		this.settingsFile.deletePath();
+	}
+
+	@Test
+	public void testSettingsFileGetProperty() {
+		this.settingsFile = new SettingsFile(this.settingsPath, this.settingsName);
+		this.settingsFile.setProperty("key1", "value1");
+		this.settingsFile.setProperty("key2", "value2");
+		String value1 = this.settingsFile.getProperty("key1");
+		String value2 = this.settingsFile.getProperty("key2");
+		assertEquals("value1", value1);
+		assertEquals("value2", value2);
+		this.settingsFile.delete();
+		this.settingsFile.deletePath();
+	}
+
+	@Test
+	public void testSettingsFileGetProperties() {
+		this.settingsFile = new SettingsFile(this.settingsPath, this.settingsName);
+		this.settingsFile.setProperty("key1", "value1");
+		this.settingsFile.setProperty("key2", "value2");
+		Hashtable properties = this.settingsFile.getProperties();
+		assertEquals(2, properties.size());
+		String value1 = properties.get("key1");
+		String value2 = properties.get("key2");
+		assertEquals("value1", value1);
+		assertEquals("value2", value2);
+		this.settingsFile.delete();
+		this.settingsFile.deletePath();
+	}
+
+	@Test
+	public void testSettingsFileGetProperties() {
+		this.settingsFile = new SettingsFile(this.settingsPath, this.settingsName);
+		this.settingsFile.setProperty("key1", "value1");
+		this.settingsFile.setProperty("key2", "value2");
+		assertTrue(this.settingsFile.save());
+		this.settingsFile.delete();
+		this.settingsFile.deletePath();
 	}
 }
